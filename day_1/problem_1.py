a963882ba69e63ecb0fb0c0c2da34bdb26f71c1a
Douwe de Haan <rinzedouwedehaan@gmail.com>
2020-12-03 00:10:08 +0100

    Renamed the input file and cleaned up some stuff

--- day_1/problem_1.py
+++ day_1/problem_1.py
@@ -1,19 +1,20 @@
-input = list(map(int, open('input_1.txt', 'r').read().split('\n')))
+input_file = list(map(int, open('input_file_1.txt', 'r').read().split('\n')))
 
-def find_first_solution(input):
-	for first_number in input:
-		for second_number in input:
+def find_first_solution(input_file):
+	for first_number in input_file:
+		for second_number in input_file:
 			if (first_number + second_number) == 2020:
 				return [first_number, second_number, (first_number * second_number)]
 
-def find_second_solution(input):
-	for first_number in input:
-		for second_number in input:
-			for third_number in input:
+def find_second_solution(input_file):
+	for first_number in input_file:
+		for second_number in input_file:
+			for third_number in input_file:
 				if (first_number + second_number + third_number) == 2020:
 					return [first_number, second_number, third_number, (first_number * second_number * third_number)]
 
 print('first solution')
-print(find_first_solution(input))
+print(find_first_solution(input_file))
 print('second solution')
-print(find_second_solution(input))
+print(find_second_solution(input_file))
+
